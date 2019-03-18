b92b9fdfefb36d9b0872e5a26ed7a8e9e93f731f
Art <semitro_8@mail.ru>
2019-03-18 18:14:18 +0300

    edges detected

--- project/src/vision/camera.py
+++ project/src/vision/camera.py
@@ -1,8 +1,10 @@
-import cv2
+import cv2, numpy
 import logging 
 
 logging.basicConfig(level=logging.DEBUG)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
+for i in range(10000):
+    pass
 
 def get_red_mask(hsv_img):
     # note that is in [0-180] and s, v in [0, 255] 
@@ -20,16 +22,17 @@ def noise_down(mask):
 while(True):
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('1',frame)
 
     hsv = cv2.medianBlur(hsv, 5) # dealing with noise
     hsv = cv2.GaussianBlur(hsv, (1,1), 0)
     
     mask = get_red_mask(hsv)
     mask = noise_down(mask)
-
+    cv2.imshow('mask', mask) 
     result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow('mask', mask)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+    cv2.imshow('in',frame)
     cv2.imshow('res', result)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
