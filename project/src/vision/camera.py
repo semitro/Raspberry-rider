a3b1b821381068ca4a80358b69d8c83450a87b48
Art <semitro_8@mail.ru>
2019-03-18 18:54:00 +0300

    detecting edges

--- project/src/vision/camera.py
+++ project/src/vision/camera.py
@@ -1,9 +1,11 @@
-import cv2, numpy
+import cv2
+import numpy as np
 import logging 
 
 logging.basicConfig(level=logging.DEBUG)
 cap = cv2.VideoCapture(2)
-for i in range(10000):
+for i in range(50000):
+    print(i*13. - i + i -i - i*12.)
     pass
 
 def get_red_mask(hsv_img):
@@ -31,9 +33,23 @@ while(True):
     cv2.imshow('mask', mask) 
     result = cv2.bitwise_and(frame, frame, mask=mask)
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+    if len(contours) != 0:
+        cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
     cv2.imshow('in',frame)
     cv2.imshow('res', result)
+#crop
+    #(x, y) = np.where(mask == 255)
+    if len(contours) > 1:
+        x, y, w, h = cv2.boundingRect(contours[0])
+        topx = x+w
+        topy = y+h
+        cropped = frame[x:topx+1, y:topy+1]# cv2.rect (0, 255, 0, -1))
+        cv2.imshow('crop', cropped)        
+   # if len(x) != 0 and len(y) !=0 :
+   #    (topx, topy) = (np.min(x), np.min(y))
+   #     (botx, boty) = (np.max(x), np.max(y))
+   #     cropped = frame[topx:botx+1, topy:boty+1]
+   #     cv2.imshow('crop', cropped)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
