b50fe6c763d0c5d7b0c6e0c5a9961da5c1a58d36
Art <semitro_8@mail.ru>
2019-04-11 17:08:47 +0300

    Engine controls are done

--- project/src/hw/engine.py
+++ project/src/hw/engine.py
@@ -1,35 +1,35 @@
 from enum import Enum
 
 class Power(Enum):
-    ZERO = 0,
-    LOW  = 2,
-    AVG  = 3,
-    MAX  = 5
-    def to_command(self):
-            return self.value
+    ZERO = 0
+    LOW  = 2
+    AVG  = 3
+    STR  = 4
+    PWR  = 5
+    MAX  = 6
+    def to_str(self):
+        return str(self.value)
 
 class Engine:
     def __init__(self):
-        self.power = Power.ZERO
+        self.power = Power.AVG
     
     def set_power(self, power):
         self.power = power;
+        print("@s" + self.power.to_str() + "@")
 
     def stop(self):
-        print("@STOP@")
+        print("@ms@")
 
     def move_forward(self):
-        print("@s" + power.to_command() + "@")
-        print("@mw@")
+        print("@s" + self.power.to_str() + "mw@")
 
     def move_back(self):
-        print("@s" + power.to_command() + "@")
-        print("@ms")
+        print("@s" + self.power.to_str() + "mx@")
 
     def rot_right(self):
-        print("@s" + power.to_command() + "@")
-        print("@ma@")
+        print("@s" + self.power.to_str() + "md@")
 
     def rot_left(self):
-        print("@s" + power.to_command() + "@")
-        print("@md@")
+        print("@s" + self.power.to_str() + "ma@")
+
